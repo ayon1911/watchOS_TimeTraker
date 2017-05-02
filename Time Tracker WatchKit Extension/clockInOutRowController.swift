a2f3d7863e31f389a89eb51af57737af9a465b54
ayon1911 <khaledrahman-ayon@Thomass-MacBook-Air.local>
2017-05-02 15:41:50 +0200

    first commit

---
+++ Time Tracker WatchKit Extension/clockInOutRowController.swift
@@ -0,0 +1,13 @@
+//
+//  clockInOutRowController.swift
+//  Time Tracker
+//
+//  Created by Khaled Rahman-Ayon on 30.04.17.
+//  Copyright © 2017 Khaled Rahman-Ayon. All rights reserved.
+//
+
+import WatchKit
+
+class clockInOutRowController: NSObject {
+
+}
